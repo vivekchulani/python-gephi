fd6d8a254118b72b64add5446b2cbcca540c19e6
Vivek Chulani <vivekchulani@Vivek-Chulanis-MacBook-Pro.local>
2018-10-16 17:48:06 -0400

    read me and initial script

---
+++ README.md
@@ -0,0 +1,1 @@
+## Python Gephi Project
---
+++ script.py
@@ -0,0 +1,5 @@
+# Import sys for arguments, http client for api calls, json and time for request limitations to api
+import sys
+import http.client
+import json
+import time
